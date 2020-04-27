e5a04bfc597bb8a7402c8c5f2ccc0f5b24f7d791
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-04-27 15:09:27 +0700

    modify structure of database

--- db_rentalmobil.sql
+++ db_rentalmobil.sql
@@ -1,98 +1,72 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 22 Mar 2020 pada 10.52
--- Versi server: 10.1.36-MariaDB
--- Versi PHP: 5.6.38
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50729
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : db_rentalmobil
 
+ Target Server Type    : MySQL
+ Target Server Version : 50729
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 27/04/2020 15:08:56
+*/
 
---
--- Database: `db_rentalmobil`
---
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for pesan
+-- ----------------------------
+DROP TABLE IF EXISTS `pesan`;
+CREATE TABLE `pesan`  (
+  `id_pesan` int(11) NOT NULL AUTO_INCREMENT,
+  `note` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `no_telp` varchar(14) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id_pesan`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
---
--- Struktur dari tabel `pesan`
---
+-- ----------------------------
+-- Records of pesan
+-- ----------------------------
 
-CREATE TABLE `pesan` (
+-- ----------------------------
+-- Table structure for pesandetail
+-- ----------------------------
+DROP TABLE IF EXISTS `pesandetail`;
+CREATE TABLE `pesandetail`  (
+  `id_detail` int(11) NOT NULL AUTO_INCREMENT,
   `id_pesan` int(11) NOT NULL,
-  `titik_jemput` varchar(100) NOT NULL,
-  `titik_antar` varchar(100) NOT NULL,
-  `tgl_penjemputan` date NOT NULL,
-  `waktu_penjemputan` time NOT NULL,
-  `kelas_mobil` varchar(100) NOT NULL,
-  `note` varchar(100) NOT NULL,
-  `no_telp` varchar(14) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `antar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `waktu` datetime(6) NULL DEFAULT NULL,
+  `price` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_detail`) USING BTREE,
+  INDEX `id_pesan`(`id_pesan`) USING BTREE,
+  CONSTRAINT `pesandetail_ibfk_1` FOREIGN KEY (`id_pesan`) REFERENCES `pesan` (`id_pesan`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Records of pesandetail
+-- ----------------------------
 
---
--- Struktur dari tabel `users`
---
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `level` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id_user`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `level` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'robby', '123', 'user');
 
---
--- Dumping data untuk tabel `users`
---
-
-INSERT INTO `users` (`id_user`, `username`, `password`, `level`) VALUES
-(1, 'robby', '123', 'user');
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `pesan`
---
-ALTER TABLE `pesan`
-  ADD PRIMARY KEY (`id_pesan`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `pesan`
---
-ALTER TABLE `pesan`
-  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
