6c49e024b5f12b025086e318ce136df36ec0aac3
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-04-29 16:14:58 +0700

    run the mailing function

--- tes.php
+++ tes.php
@@ -3,7 +3,10 @@
 include 'config/functions.php';
 
 $myfunc = new functions();
-
+$destination = [
+	["destination" => "kuta", "date" => "02-02-2020", "cost" => "$10-$20"],
+	["destination" => "lembang", "date" => "02-02-2020", "cost" => "$10-$20"],
+	["destination" => "seminyak", "date" => "02-02-2020", "cost" => "$10-$20"]
+];
+$myfunc->send_mail("d9firmansyah@gmail.com", $destination);
 ?>
-
-<p>Get Transfer</p>
