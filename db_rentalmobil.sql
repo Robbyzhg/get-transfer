e3132e756099899d1d915119d9cdf026af1364f5
Robbyzhg <zhangrobby@yandex.com>
2020-05-02 15:52:06 +0700

    database

--- db_rentalmobil.sql
+++ db_rentalmobil.sql
@@ -1,72 +1,169 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 02, 2020 at 10:51 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50729
- Source Host           : 127.0.0.1:3306
- Source Schema         : db_rentalmobil
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 50729
- File Encoding         : 65001
 
- Date: 27/04/2020 15:08:56
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Database: `db_rentalmobil`
+--
 
--- ----------------------------
--- Table structure for pesan
--- ----------------------------
-DROP TABLE IF EXISTS `pesan`;
-CREATE TABLE `pesan`  (
-  `id_pesan` int(11) NOT NULL AUTO_INCREMENT,
-  `note` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `no_telp` varchar(14) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id_pesan`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of pesan
--- ----------------------------
+--
+-- Table structure for table `mobil`
+--
 
--- ----------------------------
--- Table structure for pesandetail
--- ----------------------------
-DROP TABLE IF EXISTS `pesandetail`;
-CREATE TABLE `pesandetail`  (
-  `id_detail` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `mobil` (
+  `id_mobil` int(11) NOT NULL,
+  `merk` varchar(100) NOT NULL,
+  `jenis` varchar(100) NOT NULL,
+  `plat` varchar(10) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `merk`, `jenis`, `plat`, `gambar`) VALUES
+(4, 'Toyota', 'Inova', 'BP 1122 XD', 'Thumnail Innova.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
   `id_pesan` int(11) NOT NULL,
-  `antar` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `waktu` datetime(6) NULL DEFAULT NULL,
-  `price` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id_detail`) USING BTREE,
-  INDEX `id_pesan`(`id_pesan`) USING BTREE,
-  CONSTRAINT `pesandetail_ibfk_1` FOREIGN KEY (`id_pesan`) REFERENCES `pesan` (`id_pesan`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+  `note` varchar(100) NOT NULL,
+  `no_telp` varchar(14) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Records of pesandetail
--- ----------------------------
+--
+-- Dumping data for table `pesan`
+--
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`  (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `level` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  PRIMARY KEY (`id_user`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+INSERT INTO `pesan` (`id_pesan`, `note`, `no_telp`, `email`) VALUES
+(1, 'hatihati', '081247901041', 'dimas@gmail.com');
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES (1, 'robby', '123', 'user');
+-- --------------------------------------------------------
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Table structure for table `pesandetail`
+--
+
+CREATE TABLE `pesandetail` (
+  `id_detail` int(11) NOT NULL,
+  `id_pesan` int(11) NOT NULL,
+  `antar` varchar(255) DEFAULT NULL,
+  `waktu` datetime(6) DEFAULT NULL,
+  `price` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `pesandetail`
+--
+
+INSERT INTO `pesandetail` (`id_detail`, `id_pesan`, `antar`, `waktu`, `price`) VALUES
+(1, 1, 'Kerobokan', '2020-12-12 00:12:00.000000', '$16 - $21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id_pesan`) USING BTREE;
+
+--
+-- Indexes for table `pesandetail`
+--
+ALTER TABLE `pesandetail`
+  ADD PRIMARY KEY (`id_detail`) USING BTREE,
+  ADD KEY `id_pesan` (`id_pesan`) USING BTREE;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `pesandetail`
+--
+ALTER TABLE `pesandetail`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pesandetail`
+--
+ALTER TABLE `pesandetail`
+  ADD CONSTRAINT `pesandetail_ibfk_1` FOREIGN KEY (`id_pesan`) REFERENCES `pesan` (`id_pesan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
