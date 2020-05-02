342592c56f37bad02761bdf20b5b1494724622d0
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-05-02 23:23:59 +0700

    set lang on wait page

--- wait.php
+++ wait.php
@@ -3,10 +3,28 @@
 	<div class="row mt-5">
 		<div class="col-12">
 			<div class="jumbotron">
-			  <center><h1 class="display-4">Silahkan Tunggu Pesanan Anda</h1></center>
-			  <center><p class="lead">Silahkan Cek Pesanan di Email Anda</p></center>
+			  <center><h1 class="display-4">
+			  	<?php if ( $_SESSION["lang"] == "id" ): ?>
+				  	Silahkan Tunggu Pesanan Anda
+				<?php else: ?>
+					Please wait for your Order
+				<?php endif ?>
+			  </h1></center>
+			  <center><p class="lead">
+			  	<?php if ( $_SESSION["lang"] == "id" ): ?>
+				  	Silahkan Cek Pesanan di Email Anda
+				<?php else: ?>
+					Please check your order on your email
+				<?php endif ?>
+			  </p></center>
 			  <hr class="my-4">
-			  <center><a class="btn btn-primary btn-lg" href="index.php" role="button">Kembali</a></center>
+			  <center><a class="btn btn-primary btn-lg" href="index.php" role="button">
+			  	<?php if ( $_SESSION["lang"] == "id" ): ?>
+				  	Kembali
+				<?php else: ?>
+					Go Back
+				<?php endif ?>
+			  </a></center>
 			</div>
 		</div>
 	</div>
